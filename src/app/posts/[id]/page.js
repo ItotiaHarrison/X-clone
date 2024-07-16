14ed3900f7fae7b79e82ba107f04d241f12f89ab
Harrison Itotia <harrisonitotia@gmail.com>
2024-07-16 13:37:33 +0300

    add comments component, update postpage to display comments

--- src/app/posts/[id]/page.js
+++ src/app/posts/[id]/page.js
@@ -3,11 +3,12 @@ import { doc, getDoc, getFirestore } from "firebase/firestore";
 import { HiArrowLeft } from "react-icons/hi";
 import Link from "next/link";
 import Post from "@/components/Post";
+import Comments from "@/components/Comments";
 
 export default async function PostPage({params}) {
   const db = getFirestore(app);
   let data = {}
-  const querySnapshot = await getDoc(doc(db, 'posts, params.id'));
+  const querySnapshot = await getDoc(doc(db, 'posts', 'params.id'));
   data = {...querySnapshot.data(), id: querySnapshot.id};
   return (
     <div className="max-w-x mx-auto border-r border-l min-h-screen">
@@ -20,6 +21,7 @@ export default async function PostPage({params}) {
         <h2 className="sm:text-lg">Back</h2>
       </div>
       <Post post={data} id={data.id} />
+      <Comments id={params.id}/>
     </div>
   )
 }
